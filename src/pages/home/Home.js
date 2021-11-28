11deb974f40bfa2a58d0bcc560df3855ce0458f5
“bryson” <brysonward77@gmail.com>
2021-11-28 15:25:42 -0500

    Edited book cover img - alt

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -39,7 +39,7 @@ function Home({setId}) {
         <div key={book.id}
         class="mb-5 relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
         <div class="w-full md:w-1/3 bg-white grid place-items-center">
-          <img src={book.cover} alt="tailwind logo" class="rounded-xl" />
+          <img src={book.cover} alt="book-cover-img" class="rounded-xl" />
         </div>
           <div class="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
             <div class="flex justify-between item-center">
